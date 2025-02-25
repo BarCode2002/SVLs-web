3694b366155c854b82b9056fa9f49b70a3075171
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-02-25 20:21:12 +0100

    avanes

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -1,7 +1,7 @@
-export const ipfsUpload = 'https://web-test.com/upload';
-export const ipfsRetrieve = 'https://web-test.com/ipfs/';
-export const mongoSmartContract = 'https://web-test.com/mongo/smartcontract';
-export const mongoBrand = 'https://web-test.com/mongo/models?brand=';
-export const mongoList = 'https://web-test.com/mongo/lists?type=';
-export const mongoLanguage = 'https://web-test.com/mongo/translations?language=';
-export const indexer = 'https://web-test.com/indexer/';
+export const ipfsUpload = 'https://securevehiclelogbook.com/upload';
+export const ipfsRetrieve = 'https://securevehiclelogbook.com/ipfs/';
+export const mongoSmartContract = 'https://securevehiclelogbook.com/mongo/smartcontract';
+export const mongoBrand = 'https://securevehiclelogbook.com/mongo/models?brand=';
+export const mongoList = 'https://securevehiclelogbook.com/mongo/lists?type=';
+export const mongoLanguage = 'https://securevehiclelogbook.com/mongo/translations?language=';
+export const indexer = 'https://securevehiclelogbook.com/indexer/';
