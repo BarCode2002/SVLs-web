a6e565877db8f4b686ae44706124b9a137d86d4b
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-03-01 16:23:56 +0100

    avances

--- src/components/dashboard/filterSVLs.tsx
+++ src/components/dashboard/filterSVLs.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import styles from '../../styles/components/dashboard/filterSVLs.module.css';
 
 type FilterSVLsProps = {
@@ -10,6 +11,8 @@ type FilterSVLsProps = {
 };
 
 const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch }: FilterSVLsProps): JSX.Element => {
+
+  const { t } = useTranslation();
 
   const handleBrowseSVLByVIN = (event: { key: string; }) => {
     if (event.key === "Enter") {
@@ -25,12 +28,16 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
     setVIN(e.target.value);
   }
 
+  const handleSearch = () => {
+    
+  }
+
   return (
     <div className={styles.filterInputFieldsContainer}>
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'numMaintenances'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -38,7 +45,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={''}
+        value={'numDefects'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -46,15 +53,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
-        onChange={updateValue}
-        onKeyDown={handleBrowseSVLByVIN}
-        placeholder={placeholder}
-      />
-      <input
-        className={styles.inputField} 
-        type="text"
-        value={VIN}
+        value={'numRepairs'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -70,7 +69,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'brand'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -78,7 +77,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'model'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -86,7 +85,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'year'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -94,7 +93,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'kilometers'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -102,7 +101,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'state'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -110,7 +109,7 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'color'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
@@ -118,12 +117,64 @@ const FilterSVLs = ({ setFilterSVLs, VIN, setVIN, placeholder, search, setSearch
       <input
         className={styles.inputField} 
         type="text"
-        value={VIN}
+        value={'power'}
         onChange={updateValue}
         onKeyDown={handleBrowseSVLByVIN}
         placeholder={placeholder}
       />
-
+      <input
+        className={styles.inputField} 
+        type="text"
+        value={'shift'}
+        onChange={updateValue}
+        onKeyDown={handleBrowseSVLByVIN}
+        placeholder={placeholder}
+      />
+      <input
+        className={styles.inputField} 
+        type="text"
+        value={'fuel'}
+        onChange={updateValue}
+        onKeyDown={handleBrowseSVLByVIN}
+        placeholder={placeholder}
+      />
+      <input
+        className={styles.inputField} 
+        type="text"
+        value={'autonomy'}
+        onChange={updateValue}
+        onKeyDown={handleBrowseSVLByVIN}
+        placeholder={placeholder}
+      />
+      <input
+        className={styles.inputField} 
+        type="text"
+        value={'climate'}
+        onChange={updateValue}
+        onKeyDown={handleBrowseSVLByVIN}
+        placeholder={placeholder}
+      />
+      <input
+        className={styles.inputField} 
+        type="text"
+        value={'usage'}
+        onChange={updateValue}
+        onKeyDown={handleBrowseSVLByVIN}
+        placeholder={placeholder}
+      />
+      <input
+        className={styles.inputField} 
+        type="text"
+        value={'storage'}
+        onChange={updateValue}
+        onKeyDown={handleBrowseSVLByVIN}
+        placeholder={placeholder}
+      />
+      <button
+        className={styles.button}
+        onClick={handleSearch}>
+        {t('Dashboard.Labels.search')}
+      </button>
     </div>
   );
 }
