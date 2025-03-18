987dd3c9f7b6c8a514345b5f03aaf22c7a132329
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-03-18 10:02:54 +0100

    avances

--- src/assets/directionArrows.tsx
+++ src/assets/directionArrows.tsx
@@ -120,3 +120,33 @@ export const BottomArrowWideWhite = () => {
     </svg>
 	);
 };
+
+export const RightArrowImages = () => {
+	return (
+    <svg fill="#000000" height="18px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0">
+      </g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+        <g id="SVGRepo_iconCarrier"> 
+        <path id="XMLID_27_" d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,
+          236.161,255,240,255 s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,
+          0 c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z">
+        </path> 
+      </g>
+    </svg>
+	);
+};
+
+export const LeftArrowImages = () => {
+	return (
+    <svg fill="#000000" height="18px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350 350">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+      <g id="SVGRepo_iconCarrier"> 
+        <path id="XMLID_27_" d="M315,150H51.213l49.394-49.394c5.858-5.857,5.858-15.355,0-21.213c-5.857-5.857-15.355-5.857-21.213,0
+          l-75,75c-5.858,5.857-5.858,15.355,0,21.213l75,75c2.928,2.929,6.768,4.394,10.606,4.394s7.678-1.465,10.606-4.394
+          c5.858-5.857,5.858-15.355,0-21.213L51.213,180H315c8.284,0,15-6.716,15-15S323.284,150,315,150z">
+        </path> 
+      </g>
+    </svg>
+	);
+};
