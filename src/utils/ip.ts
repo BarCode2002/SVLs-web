da92aa77232fa170c03f6099fdc03fda6af59306
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-03-18 10:11:28 +0100

    avances

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -1,5 +1,5 @@
 export const ipfsUpload = 'http://192.168.1.240:3000/upload';
-export const ipfsRetrieve = 'http://192.168.1.240:8080/ipfs/';
+export const ipfsRetrieve = 'http://127.0.0.1:8080/ipfs/';
 export const mongoSmartContract = 'http://192.168.1.240:3000/mongo/smartcontract';
 export const mongoBrand = 'http://192.168.1.240:3000/mongo/models?brand=';
 export const mongoList = 'http://192.168.1.240:3000/mongo/lists?type=';
