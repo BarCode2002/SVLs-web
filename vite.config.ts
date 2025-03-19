799ddbe55e5d40116ddb77f13af5bac4b6f4f926
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-03-19 23:48:23 +0100

    avances

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import removeConsole from 'vite-plugin-remove-console';
+import removeConsole from 'vite-plugin-remove-console'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(), removeConsole()]
+  plugins: [react(), nodePolyfills(), removeConsole()],
+  build: {
+    target: 'esnext'
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+    target: 'esnext'
+  },
 })
