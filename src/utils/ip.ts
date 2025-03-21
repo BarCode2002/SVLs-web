aec653cdba3da30b8f8ed3e437b01276d489de2c
CodigoDeBarras11 <daniel.fernandez.forcadell@estudiantat.upc.edu>
2025-03-21 13:37:10 +0100

    avances

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -1,5 +1,5 @@
 export const ipfsUpload = 'https://securevehiclelogbook.com/api/upload';
-export const ipfsRetrieve = 'https://securevehiclelogbook.com/ipfs/ipfs/';
+export const ipfsRetrieve = 'https://securevehiclelogbook.com/ipfs/';
 export const mongoSmartContract = 'https://securevehiclelogbook.com/api/mongo/smartcontract';
 export const mongoBrand = 'https://securevehiclelogbook.com/api/mongo/models?brand=';
 export const mongoList = 'https://securevehiclelogbook.com/api/mongo/lists?type=';
