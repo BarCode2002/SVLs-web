bb1efef6e56ede8ce80bbfcf5d7af5e506f3c94c
CodigoDeBarras11 <daniel.fernandez.forcadell@gmail.com>
2025-03-23 17:06:31 +0100

    set up linux

--- src/utils/ip.ts
+++ src/utils/ip.ts
@@ -1,7 +1,7 @@
-export const ipfsUpload = 'http://192.168.1.240:3000/upload';
+export const ipfsUpload = 'http://127.0.0.1:3000/upload';
 export const ipfsRetrieve = 'http://127.0.0.1:8080/ipfs/';
-export const mongoSmartContract = 'http://192.168.1.240:3000/mongo/smartcontract';
-export const mongoBrand = 'http://192.168.1.240:3000/mongo/models?brand=';
-export const mongoList = 'http://192.168.1.240:3000/mongo/lists?type=';
-export const mongoLanguage = 'http://192.168.1.240:3000/mongo/translations?language=';
-export const indexer = 'http://192.168.1.240:3000/indexer/';
+export const mongoSmartContract = 'http://127.0.0.1:3000/mongo/smartcontract';
+export const mongoBrand = 'http://127.0.0.1:3000/mongo/models?brand=';
+export const mongoList = 'http://127.0.0.1:3000/mongo/lists?type=';
+export const mongoLanguage = 'http://127.0.0.1:3000/mongo/translations?language=';
+export const indexer = 'http://127.0.0.1:3000/indexer/';
