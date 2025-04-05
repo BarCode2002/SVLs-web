acb0daa5e2342f2ed4e58c41d8c2a06e2448d8e7
CodigoDeBarras11 <daniel.fernandez.forcadell@gmail.com>
2025-04-05 11:01:45 +0200

    walletConnect funciona

--- src/components/dashboard/connectWalletButton.tsx
+++ src/components/dashboard/connectWalletButton.tsx
@@ -1,6 +1,7 @@
 import styles from '../../styles/components/dashboard/connectWalletButton.module.css';
-import { getWallet, connectWallet } from '../../utils/wallet';
 import { useTranslation } from "react-i18next";
+import { WalletConnect, NetworkType, PermissionScopeMethods } from "@taquito/wallet-connect";
+
 
 type ConnectWalletButtonProps = {
   setMyAddress: React.Dispatch<React.SetStateAction<string | undefined>>;
@@ -11,9 +12,34 @@ const ConnectWalletButton = ({ setMyAddress }: ConnectWalletButtonProps): JSX.El
   const { t } = useTranslation();
 
   const handleConnectWallet = async () => {
-    const wallet = getWallet();
-    await connectWallet(wallet, setMyAddress);
+    //const wallet = getWallet();
+    //await connectWallet(wallet, setMyAddress);
+
+    const walletConnect = await WalletConnect.init({
+      projectId: "36686df3446a08edac4d4f3925beb311",
+      metadata: {
+        name: "YOUR_DAPP_NAME",
+        description: "YOUR_DAPP_DESCRIPTION",
+        icons: ["ICON_URL"],
+        url: "DAPP_URL",
+      },
+    });
+    await walletConnect.requestPermissions({
+      permissionScope: {
+        networks: [NetworkType.GHOSTNET],
+        methods: [
+          PermissionScopeMethods.TEZOS_SEND,
+          PermissionScopeMethods.TEZOS_SIGN,
+          PermissionScopeMethods.TEZOS_GET_ACCOUNTS
+        ],
+      },
+      registryUrl: `https://explorer-api.walletconnect.com/v3/wallets?projectId=36686df3446a08edac4d4f3925beb311`,  
+    });
+    const pkh = await walletConnect.getPKH();
+    console.log(pkh);
+
   };
+  
 
   return (
     <div>
